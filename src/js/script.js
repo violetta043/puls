2cf0034ee70309e681d9ca2a1912bfb2f5a9c3f6
Violetta <example@gmail.com>
2023-09-22 16:06:39 +0300

    fourth commit

--- src/js/script.js
+++ src/js/script.js
@@ -114,7 +114,26 @@
       });
       return false;
   });
+
+    //Smooth scroll and pageup
+    $(window).scroll(function() {
+      if($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn(); 
+      } else {
+        $('.pageup').fadeOut(); 
+      }
+    });
+
+    $("a[href=#up]").click(function(){
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
   });
+
+  new WOW().init();
+});
+
+
 
 
 
--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -8,18 +8,18 @@ require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
-// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = '';                 // Наш логин
-$mail->Password = '';                           // Наш пароль от ящика
+$mail->Username = 'testingpuls@gmail.com';                 // Наш логин
+$mail->Password = 'pkakilvpmpvmljdi';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('', 'Pulse');   // От кого письмо 
-$mail->addAddress('');     // Add a recipient
+$mail->setFrom('testingpuls@gmail.com', 'Pulse');   // От кого письмо 
+$mail->addAddress('ladefi1928@vip4e.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
